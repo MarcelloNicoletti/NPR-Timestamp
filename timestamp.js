13d46c544480baa0439f2fd107e0a596b736d90d
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-06-08 17:18:33 -0600

    Extract filename function

--- timestamp.js
+++ timestamp.js
@@ -1,41 +1,51 @@
 function onClickDownload_Btn() {
-    var hour = getHourString();
-    var minute = getMinuteString();
-    var ampm = getAmPmString();
-    var day = getDayString();
-    var month = getMonthString();
-    var date = getDateString();
     document.getElementById("download_btn").disabled = true;
 
-    console.log("Downloading...");
-    console.log(hour);
-    console.log(minute);
-    console.log(ampm);
-    console.log(day);
-    console.log(month);
-    console.log(date);
+    const fileNames = getFileNames();
+    console.log(fileNames);
+
     document.getElementById("download_btn").disabled = false;
 }
 
-function getHourString() {
+function getFileNames() {
+    const hour = getHourNumber();
+    const minute = getMinuteNumber();
+    const ampm = getAmPmString();
+    const day = getDayNumber();
+    const month = getMonthNumber();
+    const date = getDateNumber();
+    return {
+        prefixFile: "resources/prefix.mp3",
+        suffixFile: "resources/suffix.mp3",
+        onFile: "resources/on.mp3",
+        hourFile: "resources/hour_" + hour + ".mp3",
+        minuteFile: "resources/minute_" + minute + ".mp3",
+        dayFile: "resources/day_" + day + ".mp3",
+        monthFile: "resources/month_" + month + ".mp3",
+        dateFile: "resources/date_" + date + ".mp3",
+        ampmFile: "resources/" + ampm + ".mp3"
+    }
+}
+
+function getHourNumber() {
     var value = document.getElementById("ts_hour").value;
     if (!value) {
         var hours = new Date().getHours() % 12;
-        if (hours == 0) hours = 12;
+        if (hours === 0) hours = 12;
         document.getElementById("ts_hour").value = hours;
         value = hours;
     }
-    return value.toString();
+    return parseInt(value);
 }
 
-function getMinuteString() {
+function getMinuteNumber() {
     var value = document.getElementById("ts_minute").value;
     if (!value) {
         var minutes = new Date().getMinutes() % 60;
         document.getElementById("ts_minute").value = minutes;
         value = minutes;
     }
-    return value.toString();
+    return parseInt(value);
 }
 
 function getAmPmString() {
@@ -45,35 +55,35 @@ function getAmPmString() {
         document.getElementById("ts_ampm").value = (hours > 12) ? "pm" : "am";
         value = (hours > 12) ? "pm" : "am";
     }
-    return value.toString();
+    return value;
 }
 
-function getDayString() {
+function getDayNumber() {
     var value = document.getElementById("ts_day").selectedIndex;
-    if (!value || value == 0) {
+    if (!value || value === 0) {
         var day = new Date().getDay() + 1;
         document.getElementById("ts_day").selectedIndex = day;
         value = day;
     }
-    return document.getElementById("ts_day").value;
+    return parseInt(value);
 }
 
-function getMonthString() {
+function getMonthNumber() {
     var value = document.getElementById("ts_month").selectedIndex;
-    if (!value || value == 0) {
+    if (!value || value === 0) {
         var month = new Date().getMonth() + 1;
         document.getElementById("ts_month").selectedIndex = month;
         value = month;
     }
-    return value.toString();
+    return parseInt(value);
 }
 
-function getDateString() {
+function getDateNumber() {
     var value = document.getElementById("ts_date").value;
     if (!value) {
         var date = new Date().getDate();
         document.getElementById("ts_date").value = date;
         value = date;
     }
-    return value.toString();
+    return parseInt(value);
 }
